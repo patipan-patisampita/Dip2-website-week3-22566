c0c58d4ac350f16286123eb96bcaff59a0e88332
patipan-patisampita <patipan-patisampita@gmail.com>
2023-10-31 11:02:03 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const fname = "";
+const lname = "";
+const userObject = { user: "mark", pass: "1234" };
+const userArr = ["Elon", "Musk"];
+const link = "https://www.google.com";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return { fname, lname, userObject, userArr, link };
+  },
+  methods: {
+    thisArrowFunc: () => {
+      const username2 = "bill";
+      return username2 + " " + fname;
+    },
+
+    greeting: (age) => {
+      return "Hello " + fname + " " + fname + age;
+    },
+  },
+  computed: {
+    fulName() {
+      return this.fname + "" + this.lname;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div>
+    <p>Full Name: {{ fulName }}</p>
+    <p>
+      <label for="">First Name: </label>
+      <input type="text" v-model="fname" />
+    </p>
+    <p>
+      <label for="">Last Name:</label>
+      <input type="text" v-model="lname" />
+    </p>
+    <hr />
+
+    <p>{{ thisArrowFunc() }}</p>
+    <p>{{ greeting(50) }}</p>
+    <a :href="link">Google</a>
+    <h1>{{ fname }} {{ lname }}</h1>
+    <h3>User:{{ userObject.user }}</h3>
+    <h3>Pass:{{ userObject.pass }}</h3>
+    <h3>{{ userArr[0] }} {{ userArr[1] }}</h3>
+  </div>
+</template>
+
+<style></style>
